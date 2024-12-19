f9d1429c2111b24e96b2385b96fe769de65fd456
nathanstephenson <nathans@invevo.com>
2024-12-19 18:36:12 +0000

    actually export new types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { Result, SerializableResult } from "./types"
+export { Result, SuccessResult, ErrorResult, SerializableResult, SuccessSerializableResult, ErrorSerializableResult } from "./types"
 
 export { deserializeResult, parseSchema, stringify, parse, flattenErrors, tryCatch, tryCatchAsync, unwrap } from "./functions"
